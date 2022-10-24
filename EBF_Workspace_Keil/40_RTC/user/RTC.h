c11aa1e9c60b6fd8c5afcadceee839b54774e4aa
Amber <1442312382@qq.com>
2022-10-24 17:22:17 +0800

    删除尚未发布的例程

--- EBF_Workspace_Keil/40_RTC/user/RTC.h
+++
@@ -1,8 +0,0 @@
-#ifndef RTC_H_
-#define RTC_H_
-
-#include "hal_data.h"
-
-void RTC_Init(void);
-
-#endif
--- EBF_Workspace_e2s/27_GPT_Input_Capture/src/bsp_gpt.h
+++
@@ -1,8 +0,0 @@
-#ifndef BSP_GPT_H_
-#define BSP_GPT_H_
-
-#include "hal_data.h"
-
-void GPT_Init(void);
-
-#endif
