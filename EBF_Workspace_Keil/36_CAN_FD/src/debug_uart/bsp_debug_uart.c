b999a9ab966fe7c9af144506a6ff80887092d296
Amber <1442312382@qq.com>
2023-02-24 15:51:54 +0800

    add 36章Keil例程

---
+++ EBF_Workspace_Keil/36_CAN_FD/src/debug_uart/bsp_debug_uart.c
@@ -0,0 +1,82 @@
+#include "bsp_debug_uart.h"
+
+
+/* 调试串口 UART4 初始化 */
+void Debug_UART4_Init(void)
+{
+    fsp_err_t err = FSP_SUCCESS;
+    
+    err = R_SCI_UART_Open (&g_uart4_ctrl, &g_uart4_cfg);
+    assert(FSP_SUCCESS == err);
+}
+
+
+// CANFD 功能操作允许标志位
+volatile bool canfd0_senddata_enable = false;  // 允许 CANFD0 发送数据
+volatile bool canfd1_senddata_enable = false;  // 允许 CANFD1 发送数据
+
+/* 发送完成标志 */
+volatile bool uart_send_complete_flag = false;
+
+
+/* 串口中断回调 */
+void debug_uart4_callback (uart_callback_args_t * p_args)
+{
+    switch (p_args->event)
+    {
+        case UART_EVENT_RX_CHAR:
+        {
+            /* 根据字符指令进行操作 */
+            switch (p_args->data)
+            {
+                case '0':
+                    canfd0_senddata_enable = true;
+                    break;
+                case '1':
+                    canfd1_senddata_enable = true;
+                    break;
+                default:
+                    // input error
+                    break;
+            }
+            break;
+        }
+        case UART_EVENT_TX_COMPLETE:
+        {
+            uart_send_complete_flag = true;
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+
+/* 重定向 printf 输出 */
+#if defined __GNUC__ && !defined __clang__
+int _write(int fd, char *pBuffer, int size); //防止编译警告
+int _write(int fd, char *pBuffer, int size)
+{
+    (void)fd;
+    R_SCI_UART_Write(&g_uart4_ctrl, (uint8_t *)pBuffer, (uint32_t)size);
+    while(uart_send_complete_flag == false);
+    uart_send_complete_flag = false;
+
+    return size;
+}
+#else
+int fputc(int ch, FILE *f)
+{
+    (void)f;
+    R_SCI_UART_Write(&g_uart4_ctrl, (uint8_t *)&ch, 1);
+    while(uart_send_complete_flag == false);
+    uart_send_complete_flag = false;
+
+    return ch;
+}
+#endif
+
+
+
+
+
